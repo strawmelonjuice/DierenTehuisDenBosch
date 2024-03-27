ccbe00873f064723beeed68e9d8c3d43781826f9
Mar <mar@strawmelonjuice.com>
2024-03-27 10:44:45 +0100

    Add sidebar JS and inner styling

--- scripts/page.js
+++ scripts/page.js
@@ -5,4 +5,16 @@
     <Subject>
 
 */
-
+// Create nav functions
+const sidenav = {
+    close: () => {
+        document.getElementById("side-nav").style.left = `-${(document.getElementById("side-nav").offsetWidth)}px`;
+    },
+    open: () => {
+        document.getElementById("side-nav").style.left = "0";
+    }
+}
+sidenav.close()
+// Add onclick events to buttons
+document.getElementById("side-menu-opener").setAttribute("onclick", "sidenav.open()");
+document.querySelector("#side-menu-closer button").setAttribute("onclick", "sidenav.close()");
