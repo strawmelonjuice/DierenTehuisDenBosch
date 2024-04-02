a65121e251e10ddb3452059e7532ec57dbc461e1
Mar <mar@strawmelonjuice.com>
2024-04-02 12:30:22 +0200

    On resize recollapse

--- scripts/page.js
+++ scripts/page.js
@@ -5,16 +5,29 @@
     <Subject>
 
 */
-// Create nav functions
+// Creeer de navigatiefuncties
 const sidenav = {
-    close: () => {
-        document.getElementById("side-nav").style.left = `-${(document.getElementById("side-nav").offsetWidth)}px`;
-    },
-    open: () => {
-        document.getElementById("side-nav").style.left = "0";
-    }
-}
-sidenav.close()
-// Add onclick events to buttons
-document.getElementById("side-menu-opener").setAttribute("onclick", "sidenav.open()");
-document.querySelector("#side-menu-closer button").setAttribute("onclick", "sidenav.close()");
+	close: () => {
+		document.getElementById("side-nav").style.left = `-${
+			document.getElementById("side-nav").offsetWidth
+		}px`;
+	},
+	open: () => {
+		document.getElementById("side-nav").style.left = "0";
+	},
+};
+sidenav.close();
+// Maak de knopjes klikbaar
+document
+	.getElementById("side-menu-opener")
+	.setAttribute("onclick", "sidenav.open()");
+document
+	.querySelector("#side-menu-closer button")
+	.setAttribute("onclick", "sidenav.close()");
+// Bij het resizen, moet het verborgen menu ook geresized worden, anders verschijnt het alsnog op het scherm.
+window.onresize = () => {
+	// Om zeker te weten dat we alle bewegingen te pakken hebben, wachten we 100 ms.
+	setTimeout(() => {
+		sidenav.close();
+	}, 100);
+};
